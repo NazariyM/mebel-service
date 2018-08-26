47b3d6a7052e3cd6667923df8528f721c0e394f2
nazarriko@gmail.com <NazariiDev2409>
2018-08-26 14:03:14 +0300

    init

---
+++ src/js/Common.js
@@ -0,0 +1,20 @@
+import objectFitImages from 'object-fit-images';
+import 'jquery-mask-plugin';
+import './components/Sliders';
+// import './Popups';
+
+export class Common {
+  constructor() {
+    this.init();
+  }
+  init() {
+    objectFitImages();
+    this.maskInput();
+  }
+  maskInput() {
+    const $input = $('.js-tel-input');
+    $input.mask('+7 (000) 000-00-00');
+  }
+}
+
+export default new Common();
